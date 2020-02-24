c4e303b0e78ae7594bd690a91dc0e9b3db63d221
Mika Ratilainen <mihail.ratilainen@yandex.ru>
2020-02-24 23:18:32 +0300

    finished markup and css

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# news-explorer-frontend
+# NEWS EXPLORER FRONTEND
+
+Yandex Practicum diplom by Mika Ratilainen
+
+## [Ссылка на проект](https://mikaratilainen.github.io/news-explorer-frontend/)
+
+```
+npm run build
+```
+To build project
+
+```
+npm run dev
+```
+To run local server and wacth changes
+
+```
+npm run deploy
+```
+To deploy project to github pages
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const webpack = require('webpack');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
+
+module.exports = {
+    entry: {
+        main: './src/index.js',
+        savedArticles: './src/saved-articles.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+            use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
+            exclude: /node_modules/ // исключает папку node_modules
+        },
+        {
+            test: /\.css$/i,
+            use: [
+                (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+                'css-loader', 
+                'postcss-loader'
+            ]
+        },
+        {
+            test: /\.(png|jpg|gif|ico|svg)$/,
+            use: [
+                'file-loader?name=./images/[name].[ext]', // указали папку, куда складывать изображения
+                {
+                    loader: 'image-webpack-loader'
+                },
+            ],
+        },
+        {
+            test: /\.(eot|ttf|woff|woff2)$/,
+            loader: 'file-loader?name=./vendor/[name].[ext]'
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css'
+        }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+                preset: ['default'],
+            },
+            canPrint: true
+       }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: './src/saved-articles.html',
+            filename: 'saved-articles.html'
+        }),
+        new WebpackMd5Hash(),
+        new webpack.DefinePlugin({
+            'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        })
+    ]
+}
