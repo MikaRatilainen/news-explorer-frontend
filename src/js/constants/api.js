6869f6a7313a0b139df4b66300489e5fd498acae
Mika Ratilainen <mihail.ratilainen@yandex.ru>
2020-03-08 03:21:44 +0300

    minor fix

--- src/js/constants/api.js
+++ src/js/constants/api.js
@@ -1,5 +1,5 @@
 export const BASE_URL = 'https://api.ratilainen.ru';
 
-export const NEWS_API_BASE_URL = 'http://newsapi.org/v2';
+export const NEWS_API_BASE_URL = 'https://newsapi.org/v2';
 export const NEWS_API_KEY = 'b8f0d819cb464813a4b9503651168adf';
 export const NEWS_API_PAGE_SIZE = 100;
